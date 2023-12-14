c65a2ea51443254a1870d9ca1a144e9452ed733e
박시온 <sionpark2525@gmail.com>
2023-12-14 17:24:23 +0900

    Initial commit

---
+++ chap09/src/Anony/Main.java
@@ -0,0 +1,14 @@
+package Anony;
+
+public class Main {
+
+	public static void main(String[] args) {
+		RemoteControl r = new RemoteControl() {
+			public void TurnOn() {
+				System.out.println("TV is on.");
+			}
+		};
+		
+		r.TurnOn();
+	}
+}
